60212feb438a8d2f6c3f9b57f9e38fac6a260c73
Amy Keneson <amy.l.keneson@gmail.com>
2020-07-25 12:27:43 -0500

    Attempted to the startQuiz function

--- Assets/script.js
+++ Assets/script.js
@@ -1,103 +1,70 @@
-
-// ATTENTION: TIMER DOES NOT WORK
-var timer = document.querySelector("#timer");
-var seconds= document.querySelector("seconds");
-// array of seconds to 
-
-var secondsLeft = setInterval(60,0);
-
+// Grabbing seconds
+var secondsDisplay = document.querySelector("#seconds");
+var totalSeconds = 0;
+var secondsElapsed = 0;
+var interval;
 function startTimer(){
-    secondsLeft--;
-    if (secondsLeft<0){
-        // needs to stop quiz
-        console.log("out of time");
-        return;
-    } else{
-        document.getElementById('seconds').innerHTML=secondsLeft + " seconds";
-    }
+  console.log("startTimer started with start quiz button clicked")
+  console.log(totalSeconds);
+  console.log(secondsElapsed);
+  // i just really need seconds elapsed from 60 seconds
 }
+
 
 
 
 // View High Scores Variable
 var viewScores = document.querySelector("#ViewScores");
-
 // QUESTION DISPLAY BOX
-var question = document.querySelector("#questionDisplay");
-
+var questionDisplay = document.querySelector("#questionDisplay");
 // ANSWER DISPLAY BOX
 var answer = document.querySelector("#answer");
-
-
 // start button
 var startButton = document.querySelector("#startQuiz");
-
 // card-body
 var cardBody = document.querySelector("#card-body");
-
+// variable grade
 var grade = 0;
 
-var questionBank = [
-    
-    {
-        question: "Commonly used data types do NOT include",
-        option1:"boolean",
-        correct1:"numbers",
-        option2:"strings",
-        option3:"alerts"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed with ____.",
-        correct1:"parenthesis",
-        option1:"quotes",
-        option2:"curley brackets",
-        option3:"square brackets"
-    },
-    {
-        question:"Arrays in Javascript can be used to store ____.",
-        option1:"more arrays",
-        option2:"boolean",
-        option3: "numbers and strings",
-        correct1: "all of the above"
-    },
-    {
-        question:"String values must be enclosed with ____ when being assigned to variables",
-        option1:"curley brackets",
-        option2:"parenthesis",
-        correct1:"quotes",
-        option3:"commas"
-    },
-    {
-        question:"A very useful tool during development and debugging content that is for printing to the debugger is",
-        correct1:"console.log",
-        option1:"javascript",
-        option2: "terminal|gitbash",
-        option3: "loops"
-    }
-]
-
-
-function displayQuestion(){
-    question.textContent="none";
-    for (i=0; i<questionBank.length; i++){
-        console.log("loop for questions working");
-        // I need to display the object in the array
-
-    }
-    return;
+function startQuiz(){
+  console.log("Start Quiz function works");
+  // step 1: clear question
+  
+  // step 2: display question
+  questionDisplay.replaceWith("Question and answer 1 here");
+    // maybe add a show(question1);
+  // step 3: addEventListener("hover",value 2);
+    // footer shows correct();
+  // step 4: addevent Lister("hover", value 1,3,4);
+    //footer shows incorrect();
+  // step 5: addEventListern("click", value 2);
+    // next question();
+    // score = score+20;
+    // store score
+  // step 6: addEeventLister("click, value 1,3,4");
+    // -10 seconds off timer();
+    // no change to score
+  // step 7: next question(2);
+  // Step 8: repeat until question 5;
+  // step 9: addPersonToList();
 
 }
+
 
 // click starts the quiz
 startButton.addEventListener("click", function(){
     console.log("start quiz button works");
-    displayQuestion();
+  // starts timer
+    startTimer();
+  // starts quiz 
+    startQuiz();
 })
 
 // array of people
 var people =[{name:"Amy"}];
 var currentID = 0;
 
+// took this from class activities
 function addPersonToList(event){
     event.preventDefault();
     var name = nameEntered.value;
@@ -106,17 +73,14 @@ function addPersonToList(event){
     li.innerHTML = name + "<button>Add to High Score</button>";
     people.push({name:name});
     peopleListEl.append(li);
+    console.log(peopleListEl);
 }
 
 // View High Scores
 viewScores.addEventListener("click", function(){
     event.preventDefault();
     question.textContent=("View High Scores")
-
 });
 
 
-if (answer){
-    console.log("answer if statement working");
-    grade++;
-}
+// --------------------------------------------------
