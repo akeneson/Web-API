80702a4f7e01dea231302fb662cc9d4aa3a0e092
Amy Keneson <amy.l.keneson@gmail.com>
2020-07-24 19:22:18 -0500

    added variables. Start button added in html and it's addEventListener button works

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,66 @@
-console.log("js file linked")
+// QUESTION DISPLAY BOX
+var question = document.querySelector("#questionDisplay");
+
+// ANSWER DISPLAY BOX
+var answer = document.querySelector("#answer");
+
+// start button
+var startButton = document.querySelector("#startQuiz");
+
+var grade = 0;
+
+var questionBank = [
+    
+    {
+        question: "Commonly used data types do NOT include",
+        option1:"boolean",
+        correct1:"numbers",
+        option2:"strings",
+        option3:"alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ____.",
+        correct1:"parenthesis",
+        option1:"quotes",
+        option2:"curley brackets",
+        option3:"square brackets"
+    },
+    {
+        question:"Arrays in Javascript can be used to store ____.",
+        option1:"more arrays",
+        option2:"boolean",
+        option3: "numbers and strings",
+        correct1: "all of the above"
+    },
+    {
+        question:"String values must be enclosed with ____ when being assigned to variables",
+        option1:"curley brackets",
+        option2:"parenthesis",
+        correct1:"quotes",
+        option3:"commas"
+    },
+    {
+        question:"A very useful tool during development and debugging content that is for printing to the debugger is",
+        correct1:"console.log",
+        option1:"javascript",
+        option2: "terminal|gitbash",
+        option3: "loops"
+    }
+]
+
+function startQuiz(){
+    console.log("start quiz button works");
+}
+
+startButton.addEventListener("click", startQuiz());
+
+
+
+for (i=0; i<6; i++){
+    console.log("loop for questions working");
+}
+
+if (answer){
+    console.log("answer if statement working");
+    grade++;
+}
