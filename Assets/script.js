8812b92cd0d4264785611e99f48a0b4dee07d0c9
Amy Keneson <amy.l.keneson@gmail.com>
2020-07-25 21:10:54 -0500

    Started javascript page over

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,69 @@
+// click starts the quiz
+var startQuizButton = document.getElementById("startQuiz");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+var question = document.getElementById("question");
+
+var questionBank = [{
+    question: "Commonly used data types do NOT include",
+    correct: "Alert",
+    choices: ["Boolean", "strings", "numbers"],
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with ____.",
+    correct: "parenthesis",
+    choices: ["curley brackets","quotations","parenthesis","square brackets"]
+  },
+  {
+    question: "Arrays in Javascript can be used to store ____.",
+    correct: "all of the above", 
+    choices: ["more arrays","boolean","numbers and strings","all of the above"]
+  },
+  {
+    question: "A very useful tool during development and debugging content that is for printing to the debugger is ________.",
+    correct:"console.log",
+    choices: ["console.log","javascript","terminal | get bash","loops",]
+  },
+  {
+    question: "String values must be enclosed with ____ when being assigned to variables.",
+    correct:"quotes",
+    choices: ["curley brackets","parethesis","quotes","commas"]
+  }
+  ];
+
+
+startQuizButton.addEventListener("click", function(){
+  console.log("Start Quiz button works");
+  startTimer();
+  startQuiz();
+});
+
+function startQuiz(){
+    console.log("startQuiz function working");
+    footer.replaceWith("Pick an answer.");
+    questionDisplay.replaceWith("Questions to show here");
+    for (i=0; i<questionBank.length; i++){
+        console.log("test");
+    }
+
+    
+
+}
+
+
+
+
+
+
+// -----------------------------TIMER-------------------------------
 // Variables for timer
 var secondsDisplay = document.querySelector("#seconds");
 var question1 = document.querySelector("question1");
 var totalSeconds = 60;
 var interval;
+
 
 function startTimer(){
   event.preventDefault();
@@ -28,113 +89,23 @@ function displayTime(){
   // -10 seconds
 }
 
-var grade = 0;
-var questionNumber=0;
-var rightAnswer = "";
-var userAnswer ="";
-
-var questionArray = [{
-  question: "Commonly used data types do NOT include",
-  correct: "Alert",
-  choices: ["Boolean","alert", "strings", "numbers"],
-},
-{
-  question: "The condition in an if/else statement is enclosed with ____.",
-  correct: "parenthesis",
-  choices: ["curley brackets","quotations","parenthesis","square brackets"]
-}
-];
-
-// click starts the quiz
-var startQuizButton = document.getElementById("startQuiz");
-var introContainer = document.getElementById("intro");
-
-startQuizButton.addEventListener("click", function(){
-  console.log("Start Quiz button works");
-  startTimer();
-  startQuiz();
-});
-
-function startQuiz(){
-  console.log("startQuiz Function is working");
-  footer.replaceWith("Pick an answer.");
-
-  // checks to makes sure that startQuiz() keeps running until out of questions
-  if (questionNumber<questionArray.length){
-    console.log("if statement in startQuiz working");
-    // loop for the array to run through all the questions
-    questionDisplay.replaceWith(document.getElementById("question").innerHTML=questionArray[questionNumber].question);
-    console.log("FIRST: DISPLAY QUESTION");
-      // for loop will create all the buttons
-      for (i=0; i<questionArray[questionNumber].choices.length; i++){
-        // SECOND: make buttons for answers
-        rightAnswer = questionArray[questionNumber].correct;
-        let answerButton = document.createElement("button");
-        answerButton.setAttribute("class", "btn-choice m-4 btn", "id=answers");
-        answerButton.textContent = questionArray[questionNumber].choices[i];
-        document.getElementById("answers").append(answerButton);
-        console.log("SECOND: DISPLAY BUTTONS");
-        
-        
-      }
-    
-  }
-  
-}
-
-
-
-
-
-  // step 1: clear question
-  // questionDisplay.remove("");
-  // step 2: display question
-  // questionDisplay.add();
-  // answer.replaceWith("");
-    // maybe add a show(question1);
-  // step 3: addEventListener("hover",value 2);
-    // footer shows correct();
-  // step 4: addevent Lister("hover", value 1,3,4);
-    //footer shows incorrect();
-  // step 5: addEventListern("click", value 2);
-    // next question();
-    // score = score+20;
-    // store score to localStorage
-  // clear screen
-  // step 6: addEeventLister("click, value 1,3,4");
-    // -10 seconds off timer(); To do this you will need to take the seconds at that instance, -10, and then redisplay seconds.
-    // no change to score
-  // step 7: next question(2);
-  // Step 8: repeat until question 5;
-  // step 9: addPersonToList();
-  
-  // ^^loop for the above
-
-  // when the loop ends, go to high score page
-  // add input field
-  // add button | also make sure this stores to localStorage
-  // clear button | also make sure this clears localStorage
-
-
-
-
-
+// --------------------------View High Scores -------------------------
 function addName(){
-// took this from class activities
-// 1) make an array of objects
-// 2) var highScoreArray = [];
-// 3) add person to array
-// 4) 
-}
-
-// View High Scores
-$("#ViewScores").on("click", function(){
-  event.preventDefault();
-  questionDisplay.replaceWith("View High Scores");
-  // how to display a div class, or an string of html?
-  // is there a function to go back to the main page?
-  // localStorage.setItem(""); PLACE TO LOCAL STRAOGE
-
-  footer.replaceWith("");
-  
-});
+    // took this from class activities
+    // 1) make an array of objects
+    // 2) var highScoreArray = [];
+    // 3) add person to array
+    // 4) 
+    }
+    
+    // View High Scores
+    $("#ViewScores").on("click", function(){
+      event.preventDefault();
+      questionDisplay.replaceWith("View High Scores");
+      // how to display a div class, or an string of html?
+      // is there a function to go back to the main page?
+      // localStorage.setItem(""); PLACE TO LOCAL STRAOGE
+    
+      footer.replaceWith("");
+      
+    });
